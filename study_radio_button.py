532b1a26eae6812b132715c553c36c1c1733e277
IvanovSemjon <IvanovSemjon@yabdex.ru>
2025-08-18 10:52:06 +0300

    Скорректирован шоубокс

--- study_radio_button.py
+++ study_radio_button.py
@@ -1,21 +1,22 @@
 import tkinter as tk
 from tkinter import messagebox
 
+def study_radio_button():
+    '''Изучение радиокнопок'''
+    window = tk.Toplevel()
+    window.title('Радиокнопки')
+    window.geometry('300x200')
 
-window = tk.Tk()
-window.title('Радиокнопки')
-window.geometry('300x200')
+    var = tk.StringVar(value='Python')
 
-var = tk.StringVar(value='Python')
+    tk.Label(window, text = 'Выберите язык: ').pack(pady=5)
+    tk.Radiobutton(window, text='Python', variable=var, value='Python').pack()
+    tk.Radiobutton(window, text='Java', variable=var, value='Java').pack()
+    tk.Radiobutton(window, text='C++', variable=var, value='C++').pack()
 
-tk.Label(window, text = 'Выберите язык: ').pack(pady=5)
-tk.Radiobutton(window, text='Python', variable=var, value='Python').pack()
-tk.Radiobutton(window, text='Java', variable=var, value='Java').pack()
-tk.Radiobutton(window, text='C++', variable=var, value='C++').pack()
 
-def show_value():
-    messagebox.showinfo("Результат", f"Выбран: {var.get()}")
+    def show_choice():
+        messagebox.showinfo("Результат", f"Выбран: {var.get()}")
 
-tk.Button(window, text='Показать', command=show_value).pack(pady=10)
+    tk.Button(window, text='Показать', command=show_choice).pack(pady=10)
 
-window.mainloop()
