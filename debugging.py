d84248637e099bfd4948a33a7a309fd30fd4531b
IvanovSemjon <IvanovSemjon@yabdex.ru>
2025-08-19 13:17:34 +0300

    Настроили colorlog

--- debugging.py
+++ debugging.py
@@ -1,10 +1,26 @@
 import logging
+import colorlog
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-logging.debug('Подробная информация для отладки')   # DEBUG
-logging.info('Информационное сообщение')            # INFO
+handler = colorlog.StreamHandler()
+handler.setFormatter(colorlog.ColoredFormatter(
+    '%(log_color)s%(asctime)s - %(levelname)s - %(message)s',
+    log_colors={
+        'DEBUG': 'cyan',
+        'INFO': 'green',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'red,bg_white',
+    }
+))
 
-logging.warning('Предупреждение об ошибке')         # WARNING
-logging.error('Ошибка в программе')                 # ERROR
-logging.critical('Критическая ошибка')              # CRITICAL
+logger = colorlog.getLogger()
+logger.addHandler(handler)
+
+logger.debug('Подробная информация для отладки')   # DEBUG
+logger.info('Информационное сообщение')            # INFO
+
+logger.warning('Предупреждение об ошибке')         # WARNING
+logger.error('Ошибка в программе')                 # ERROR
+logger.critical('Критическая ошибка')              # CRITICAL
