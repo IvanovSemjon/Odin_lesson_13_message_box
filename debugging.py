3c941e8baca25c5eb364ac7d3cf74ee429e5ac35
IvanovSemjon <IvanovSemjon@yabdex.ru>
2025-08-21 10:57:52 +0300

    Линтер+Дебаг

--- debugging.py
+++ debugging.py
@@ -17,11 +17,44 @@ handler.setFormatter(colorlog.ColoredFormatter(
 logger = colorlog.getLogger('example')
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
-logger.propagate = False # Отключаем передачу в родительский лог
 
-logger.debug('Подробная информация для отладки')   # DEBUG
-logger.info('Информационное сообщение')            # INFO
+# logger.debug('Подробная информация для отладки')   # DEBUG
+# logger.info('Информационное сообщение')            # INFO
+# logger.warning('Предупреждение об ошибке')         # WARNING
+# logger.error('Ошибка в программе')                 # ERROR
+# logger.critical('Критическая ошибка')              # CRITICAL
 
-logger.warning('Предупреждение об ошибке')         # WARNING
-logger.error('Ошибка в программе')                 # ERROR
-logger.critical('Критическая ошибка')              # CRITICAL
+
+from typing import Union
+
+def add_numbers(a: Union[int, str, float], b: Union[int, str, float]) -> Union[float, None]:
+    """Функция с примерами логирования"""
+    logger.debug(f"Начало выполнения add_bumbers с параметрами a={a}, b={b}")
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        try:
+            logger.warning(f'Попытка преобразовать к числу: {a}={type(a)}, {b}={type(b)}')
+            a = float(a)
+            b = float(b)
+        except:
+            logger.critical(f'Не удалось преобразовать к числу: {a}={type(a)}, {b}={type(b)}')
+            return None
+    logger.info(f"Сложение чисел {a} и {b}")
+    return a + b
+
+logger.debug('Программа запущена')
+result = add_numbers(5, 3)
+logger.info(f"Результат сложения: {result}")
+
+result = add_numbers(5, '30')
+logger.info(f"Результат сложения: {result}")
+
+result = add_numbers(5, 'hello')
+logger.info(f"Результат сложения: {result}")
+
+name = 'John'
+
+def add_a_b(a: int, b: int) -> float:
+    """Функция с примерами логирования"""
+    return a + b
+
+result = add_a_b(5, 10)
